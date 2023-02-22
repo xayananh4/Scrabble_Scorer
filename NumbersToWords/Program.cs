0f9e8da1811466b19b41b91453f384877903cc43
Viradeth Xay-Ananh <xayananh4@gmail.com>
2023-02-22 10:03:47 -0800

    add UI for paper rock game.  further exploration covered
    Co-Authored-By: Michael Sol <Sol.mike.j@gmail.com>

--- NumbersToWords/Program.cs
+++ NumbersToWords/Program.cs
@@ -13,12 +13,28 @@ class Program
 
     Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
     Console.WriteLine("Welcome to the Rock Paper Scissors App!");
-    Console.WriteLine("Please enter in a word:");
-    string targetWord = Console.ReadLine();
-    Word myWord = new Word(targetWord);
-    Console.WriteLine("Your score is: " + myWord.calScore());
+    Console.WriteLine("Player 1 enter in a choice:");
+    string choice1 = Console.ReadLine();
+    RPS player1 = new RPS(choice1);
+    Random rand = new Random();
+    int randomInt = rand.Next(3);
+    string compChoice = "error";
+    if(randomInt == 0)
+    {
+      compChoice = "rock";
+    }
+    else if(randomInt == 1)
+    {
+      compChoice = "paper";
+    }
+    else if(randomInt == 2)
+    {
+      compChoice = "scissors";
+    }
 
-
-
+    RPS compPlayer = new RPS(compChoice);
+    Console.WriteLine("Computer choice is: " + compChoice);
+    string result = player1.calOutcome(compPlayer);
+    Console.WriteLine("So the result is: " + result);
   }
 }
