6bb7ae2eab5d267c8d3809778b55a05cb0e864ba
Viradeth Xay-Ananh <xayananh4@gmail.com>
2023-02-22 09:12:25 -0800

    allow user to enter in target word in UI
    Co-Authored-By: Michael Sol<Sol.mike.j@gmail.com>

--- NumbersToWords/Program.cs
+++ NumbersToWords/Program.cs
@@ -1,7 +1,14 @@
+using NumbersToWords.Models;
+using System;
 class Program
 {
   static void Main()
   {
-    // We'll add user interface code here.
+    Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+    Console.WriteLine("Welcome to the Scrabble app!");
+    Console.WriteLine("Please enter in a word:");
+    string targetWord = Console.ReadLine();
+    Word myWord = new Word(targetWord);
+    Console.WriteLine("Your score is: " + myWord.calScore());
   }
 }
